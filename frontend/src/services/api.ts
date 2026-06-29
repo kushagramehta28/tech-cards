448d15401d1267131d61060d89d3fe2ab0be641a
Kushagra Mehta <kushagramehtamfs@gmail.com>
2026-06-29 16:39:41 +0530

    Using VITE_API_URL env var for frontend deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,7 +5,7 @@ import type { BookmarkResponse, BookmarkItem } from "../types/bookmark"
 import type { LoginRequest, SignupRequest, AuthResponse } from "../types/auth"
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
 })
 
 
